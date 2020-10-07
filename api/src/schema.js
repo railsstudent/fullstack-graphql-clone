ffb35cd9eea2254c24dd57bd69311bdb0242a452
Connie Leung <cleung334@gmail.com>
2020-10-08 00:17:52 +0800

    query type exercise

---
+++ api/src/schema.js
@@ -0,0 +1,25 @@
+const { gql } = require('apollo-server')
+
+/**
+ * Type Definitions for our Schema using the SDL.
+ */
+const typeDefs = gql`
+  type User {
+      id: ID!,
+      username: String!
+  }
+
+  type Pet {
+    id: ID!,
+    createdAt: String!,
+    name: String!,
+    type: String!    
+  }
+
+  type Query {
+    user: User!,
+    pets: [Pet!]
+  }
+`;
+
+module.exports = typeDefs
---
+++ client/src/client.js
@@ -0,0 +1,5 @@
+import { ApolloClient, InMemoryCache, HttpLink, gql } from '@apollo/clientt'
+
+/**
+ * Create a new apollo client and export as default
+ */
